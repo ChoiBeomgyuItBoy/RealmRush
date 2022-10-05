d39074b21e45aea1246e7b0b226824ed8741497f
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2022-10-05 14:54:27 -0500

    Commit

--- Assets/Tile/Waypoint.cs
+++ Assets/Tile/Waypoint.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject towerPrefab;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] bool isPlaceable;
+
+    void OnMouseDown()
     {
-        
+        if(isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
+        }
     }
 }
