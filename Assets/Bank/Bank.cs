353b76c0b78249c0ae4b604153854f697c9ac962
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2022-10-05 21:55:59 -0500

    Currency System pt.2

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     [SerializeField] int startingBalance = 150;
     
     [SerializeField] int currentBalance;
@@ -12,15 +16,35 @@ public class Bank : MonoBehaviour
     void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = $"Gold: {currentBalance}";
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
+        
+        if(currentBalance < 0)
+        {
+            ReloadScene();
+        }
+    }
+
+    public void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
