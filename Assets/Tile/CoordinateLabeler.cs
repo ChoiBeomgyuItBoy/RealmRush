6e6e9ba320403bb76c2d42b217c6f74c7bdc3696
ChoiBeomgyuItBoy <octaviortegan@hotmail.com>
2022-10-06 22:23:27 -0500

    Tunning

--- Assets/Tile/CoordinateLabeler.cs
+++ Assets/Tile/CoordinateLabeler.cs
@@ -29,6 +29,7 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
+            label.enabled = true;
         }
 
         SetLabelColor();
